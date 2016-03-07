b2dca93fff11456921d33856bd6e1ab1b4b282ee
Danielle Kenwood <dkenwood@>
2016-03-06 23:38:51 -0800

    Finished

--- App/mobile/src/main/java/com/example/skylights/represent/PhoneListenerService.java
+++ App/mobile/src/main/java/com/example/skylights/represent/PhoneListenerService.java
@@ -27,28 +27,6 @@ public class PhoneListenerService extends WearableListenerService {
         Log.d("T", "about to start watch MainActivity with SENT_ZIP");
         startActivity(intent);
         if( messageEvent.getPath().equalsIgnoreCase("/94704") ) {
-//
-////            // Value contains the String we sent over in WatchToPhoneService, "good job"
-//            String value = new String(messageEvent.getData(), StandardCharsets.UTF_8);
-//
-//            Intent intent = new Intent(this, DisplayZipActivity.class);
-//            intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
-//            //you need to add this flag since you're starting a new activity from a service
-//            intent.putExtra("zip_code", value);
-//            Log.d("T", "about to start watch MainActivity with SENT_ZIP");
-//            startActivity(intent);
-////
-////            // Make a toast with the String
-////            Context context = getApplicationContext();
-////            int duration = Toast.LENGTH_SHORT;
-////
-////            Toast toast = Toast.makeText(context, value, duration);
-////            toast.show();
-//
-//            // so you may notice this crashes the phone because it's
-//            //''sending message to a Handler on a dead thread''... that's okay. but don't do this.
-//            // replace sending a toast with, like, starting a new activity or something.
-//            // who said skeleton code is untouchable? #breakCSconceptions
 
         } else {
             super.onMessageReceived( messageEvent );
